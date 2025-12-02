25e875d3ac7004e99462074f4d0474ca906bb381
Pavel Hladkikh <hladkikh94@gmail.com>
2025-12-02 16:37:06 +0100

    Initial Commit

---
+++ TravelSchedule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelSchedule
+//
+//  Created by Admin on 02/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Admin on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
